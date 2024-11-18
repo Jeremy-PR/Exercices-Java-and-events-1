277e7ef538800dfc761a5cc3e044335677178504
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-18 14:13:16 +0100

    exo 2 done

---
+++ Exercice_2/script.js
@@ -0,0 +1,10 @@
+const lastname = document.querySelector('#lastname');
+
+lastname.addEventListener('blur', afficherMessage);
+
+
+function afficherMessage() {
+    alert("Merci de votre participation");
+}
+
+
