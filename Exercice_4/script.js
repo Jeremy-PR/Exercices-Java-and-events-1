1c5751fd4a73afaec66ed4890d988bce17d46634
Jeremy-PR <jeremy.pelisse@gmail.com>
2024-11-19 10:03:03 +0100

    Correction

--- Exercice_4/script.js
+++ Exercice_4/script.js
@@ -1,14 +1,29 @@
-const lastname = document.querySelector('#lastname');
-const firstname = document.querySelector('#firstname');
-const city = document.querySelector('#city');
-const btn = document.querySelector('#btn');
+// const lastname = document.querySelector('#lastname');
+// const firstname = document.querySelector('#firstname');
+// const city = document.querySelector('#city');
+// const btn = document.querySelector('#btn');
 
-btn.addEventListener("click",deleteData);
+// btn.addEventListener("click",deleteData);
 
-function deleteData(){
-    lastname.value = "";
-    firstname.value = "";
-    city.value = "";
+// function deleteData(){
+//     lastname.value = "";
+//     firstname.value = "";
+//     city.value = "";
   
-    
+// }
+
+
+// correction 
+
+const raz = document.querySelector("#raz");
+
+raz.addEventListener("click", handleClickRaz);
+
+function handleClickRaz() {
+  const inputs = document.querySelectorAll("input");
+
+  
+  inputs.forEach((input) => {
+    input.value = "";
+  });
 }
--- Exercice_6/script.js
+++ Exercice_6/script.js
@@ -1,27 +1,20 @@
-// const images = document.querySelectorAll('images')
-
-
-
-// images.forEach(image => {
-//     image.addEventListener('mouseover', handleAddImage)
-     
-// });
-
-
-correction Exo 5
-
 const images = document.querySelectorAll("#container > img");
 
 console.log(images);
 
 images.forEach((image) => {
     image.addEventListener("mouseenter", handleMouseEnter);
+    image.addEventListener("mouseleave", handleMouseLeave);
 });
 
 function handleMouseEnter(event){
     const imgTag = event.target;
 
     imgTag.src = imgTag.src.replace('.jpg', '_2.jpg');
+}
 
-    imgTag.removeEventListener("mouseenter", handleMouseEnter);
+function handleMouseLeave(event){
+    const imgTag = event.target;
+
+    imgTag.src = imgTag.src.replace('_2.jpg', '.jpg');
 }
